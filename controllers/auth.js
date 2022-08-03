605ddad30f69d3b8ea93008a18dbd15a8a3089c6
Samuel Castro <cassam05@hotmail.com>
2022-08-03 12:18:21 -0700

    Google Sign In added

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require("express");
 const Usuario = require('../models/usuario')
 const bcryptjs = require('bcryptjs');
 const { generarJWT } = require("../helpers/generar-JWT");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req, res= response)=>{
     
@@ -56,6 +57,56 @@ const login = async (req, res= response)=>{
     
 }
 
+
+const googleSignIn = async(req,res = response) =>{
+    
+    const {id_token} = req.body;
+
+    try {
+        
+        const {nombre, img, correo} = await googleVerify(id_token);
+        
+        let usuario = await Usuario.findOne({correo});
+
+        if(!usuario){
+            //Crearlo
+            const data = {
+                nombre,
+                correo,
+                password:'0',
+                rol:'ADMIN_ROLE',
+                google:true,
+                img
+            }
+
+            usuario = new Usuario(data);
+            usuario.save();
+        }
+
+        if(!usuario.estado){
+            return res.status(401).json({
+                msg:'Hable con el admin - user bloqueado'
+            })
+        }
+
+        const token = await generarJWT(usuario.id);
+
+        //console.log(goolgeUser)
+        
+        res.json({
+            usuario,
+            token
+        })
+    } catch (error) {
+        res.status(400).json({
+            msg:'Token no se pudo verificar',
+            id_token
+        })
+    }
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
